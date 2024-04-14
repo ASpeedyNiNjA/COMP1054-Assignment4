4579a2495ddb0ccc4cc11423d4b0e83dab2b5db3
ASpeedyNiNjA <christrombly001@hotmail.com>
2024-04-14 16:08:01 -0400

    First row of footer seperated into 3 columns with CSS grids

--- shared/footer.php
+++ shared/footer.php
@@ -1,6 +1,18 @@
 	<br>
 	<footer>
-		<small>All copyrights&copy; belong to the DLC!</small>
+		<div id="language">
+			<p>Language:
+				<br>
+				English/French</p>
+		</div>
+		<div id="copyright">
+			<small>All copyrights&copy; belong to the DLC!</small>
+		</div>
+		<div id="company">
+			<p>DemonsLoveCompany
+			<br>
+			Ministry Of Love</p>
+		</div>
 	</footer>
 </body>
 </html>
