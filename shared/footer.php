1c75d622b5ec9c3d014d031122a437463968d8bc
ASpeedyNiNjA <christrombly001@hotmail.com>
2024-04-14 16:27:10 -0400

    Relative positioning added to move  Skull-Straw logo in footer closer to copyright fineprint.

--- shared/footer.php
+++ shared/footer.php
@@ -7,6 +7,7 @@
 		</div>
 		<div id="copyright">
 			<small>All copyrights&copy; belong to the DLC!</small>
+			<img src="images/skull-straw-2.png" alt="skull-straw DLC logo" id="skull-straw">
 		</div>
 		<div id="company">
 			<p>DemonsLoveCompany
